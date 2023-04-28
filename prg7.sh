1de7dd6594e38afedc7192e868ad34ff85fc5c1d
prem <kandulaprem67@gmail.com>
2023-04-28 16:12:29 +0530

    the file modification with fie prg7.sh

--- prg7.sh
+++ prg7.sh
@@ -2,6 +2,8 @@
 #script for the arithmetic operations
 #
 read -p "Enter the first number:" num1
+
+echo "do the push ncomcepdewhciuwhc jciwnc in the git bash to git hub"
 read -p "Enter the second number:" num2
 z=$(($num1+$num2))
 echo "The addition is: $z"
